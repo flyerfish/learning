7da9c0d4a698fdcae1b7ef3fa07a851b63e14c54
chaos-god <chaos-god@users.noreply.github.com>
2020-09-16 23:50:22 +0800

    update rob hourse

--- src/main/java/com/sylar/leetcode/dp/RobHourse1.java
+++ src/main/java/com/sylar/leetcode/dp/RobHourse1.java
@@ -7,28 +7,28 @@ import java.util.Arrays;
  */
 public class RobHourse1 {
 
-    private int[] memo;
+	private int[] dp;
 
-    public int rob(int[] nums) {
-        // 初始化备忘录
-        memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        // 强盗从第 0 间房子开始抢劫
-        return dp(nums, 0);
-    }
+	public int rob(int[] nums) {
+		// 初始化备忘录
+		dp = new int[nums.length];
+		Arrays.fill(dp, -1);
+		// 强盗从第 0 间房子开始抢劫
+		return dp(nums, 0);
+	}
 
-    // 返回 dp[start..] 能抢到的最大值
-    private int dp(int[] nums, int start) {
-        if (start >= nums.length) {
-            return 0;
-        }
-        // 避免重复计算
-        if (memo[start] != -1) {
-            return memo[start];
-        }
+	// 返回 dp[start..] 能抢到的最大值
+	private int dp(int[] nums, int start) {
+		if (start >= nums.length) {
+			return 0;
+		}
+		// 避免重复计算
+		if (dp[start] != -1) {
+			return dp[start];
+		}
 
-        // 记入备忘录
-        memo[start] = Math.max(dp(nums, start + 1), nums[start] + dp(nums, start + 2));
-        return memo[start];
-    }
+		// 记入备忘录
+		dp[start] = Math.max(dp(nums, start + 1), nums[start] + dp(nums, start + 2));
+		return dp[start];
+	}
 }
