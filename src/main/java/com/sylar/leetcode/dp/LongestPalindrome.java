a9bb62667fcf97c142691c309b75b8ee090780b3
chaos-god <chaos-god@users.noreply.github.com>
2020-09-17 02:15:55 +0800

    chonggou

--- src/main/java/com/sylar/leetcode/dp/LongestPalindrome.java
+++ src/main/java/com/sylar/leetcode/dp/LongestPalindrome.java
@@ -1,5 +1,8 @@
 package com.sylar.leetcode.dp;
 
+/**
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/
+ */
 public class LongestPalindrome {
 	public String longestPalindrome(String s) {
 		if (s.length() < 2) {
@@ -34,5 +37,32 @@ public class LongestPalindrome {
 		}
 
 		return s.substring(begin, begin + maxLen);
+	}
+
+	public String longestPalindrome2(String s) {
+		if (s == null || s.length() < 1) {
+			return "";
+		}
+		int start = 0;
+		int end = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int len1 = expandAroundCenter(s, i, i);
+			int len2 = expandAroundCenter(s, i, i + 1);
+			int len = Math.max(len1, len2);
+			if (len > end - start) {
+				start = i - (len - 1) / 2;
+				end = i + len / 2;
+			}
+		}
+		return s.substring(start, end + 1);
+	}
+
+	private int expandAroundCenter(String s, int left, int right) {
+		char[] cs = s.toCharArray();
+		while (left >= 0 && right < s.length() && cs[left] == cs[right]) {
+			left--;
+			right++;
+		}
+		return right - left - 1;
 	}
 }
